1c1be1185db57b56d57765330b648097f31435ff
Natanael Lima <taelima1997@gmail.com>
2021-02-23 15:37:50 -0300

    add components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,26 @@
+import Head from 'next/head';
+import CompletedChanllanges from '../components/CompletedChanllanges';
+import CountDown from '../components/CountDown';
 import ExperienceBar from '../components/ExperienceBar';
+import Profile from '../components/Profile';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
+      <Head>
+        <title>Início | moveit</title>
+      </Head>
       <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChanllanges />
+          <CountDown />
+        </div>
+        <div />
+      </section>
     </div>
   );
 }
