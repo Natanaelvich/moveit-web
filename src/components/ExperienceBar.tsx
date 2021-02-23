d9e92eb5ff8ee778b0f78c8de919af1a2005f11f
Natanael Lima <taelima1997@gmail.com>
2021-02-23 11:59:50 -0300

    export project to figma

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from '../styles/ExperienceBar.module.css';
+
+export default function ExperienceBar() {
+  return (
+    <header className={styles.experienceBar}>
+      <span>0 xp</span>
+      <div>
+        <div style={{ width: '50%' }} />
+
+        <span className={styles.currentExperience} style={{ left: '50%' }}>
+          300 xp
+        </span>
+      </div>
+      <span>600 xp</span>
+    </header>
+  );
+}
