0103f326ad637f9c3c5dc285bdc6652cff86ad40
Natanael Lima <taelima1997@gmail.com>
2021-02-23 18:11:05 -0300

    create countdown feauture

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../styles/CountDown.module.css';
 
 export default function CountDown() {
+  const [time, setTime] = useState(25 * 60);
+  const [activeCount, setActiveCount] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+  function startCountDown() {
+    setActiveCount(true);
+  }
+
+  useEffect(() => {
+    if (activeCount && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    }
+  }, [activeCount, time]);
+
   return (
     <div>
       <div className={styles.container}>
         <div>
-          <span>2</span>
-          <span>5</span>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
         </div>
         <span>:</span>
         <div>
-          <span>2</span>
-          <span>5</span>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
         </div>
       </div>
-      <button type="button" className={styles.startCountDownButton}>
+      <button
+        onClick={startCountDown}
+        type="button"
+        className={styles.startCountDownButton}
+      >
         Iniciar um clico
       </button>
     </div>
