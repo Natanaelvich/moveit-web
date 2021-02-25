b1e62f2b4cf2934cf9c139ed8d6939c1f2929531
Natanael Lima <taelima1997@gmail.com>
2021-02-25 10:57:23 -0300

    add countDownProvider

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { ChallengeProvider } from './modules/ChallengeContext';
+import { CountDownProvider } from './modules/CountDownContext';
 
 const AppProvider: React.FC = ({ children }) => (
-  <ChallengeProvider>{children}</ChallengeProvider>
+  <ChallengeProvider>
+    <CountDownProvider>{children}</CountDownProvider>
+  </ChallengeProvider>
 );
 
 export default AppProvider;
